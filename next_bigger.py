9b12245d0e48b11d7bfb0f92685bfe1cd5ab0039
Mohamed Sharif <44378587+msharif16@users.noreply.github.com>
2021-03-12 20:45:00 +0000

    Create next_bigger.py

---
+++ next_bigger.py
@@ -0,0 +1,8 @@
+def next_bigger(n):
+    max_num = int(''.join(sorted(list(str(n)))[::-1]))
+    x = n
+    while n < max_num:
+        x += 1
+        if sorted(list(str(x))) == sorted(list(str(n))):
+            return x
+    return -1
